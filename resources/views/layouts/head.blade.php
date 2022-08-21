ac242e801f03a3ef20e20ae618068b139f333f1c
unknown <ahmeedhassanali@outlook.com>
2022-08-21 14:43:18 +0200

    css

--- resources/views/layouts/head.blade.php
+++ resources/views/layouts/head.blade.php
@@ -1,21 +1,22 @@
 <!-- Title -->
 <title> @yield("title") </title>
 <!-- Favicon -->
-<link rel="icon" href="{{asset('public/assets/img/brand/favicon.png')}}" type="image/x-icon"/>
+<link rel="icon" href="{{asset('assets/img/brand/favicon.png')}}" type="image/x-icon"/>
 <!-- Icons css -->
-<link href="{{asset('public/assets/css/icons.css')}}" rel="stylesheet">
+<link href="{{asset('assets/css/icons.css')}}" rel="stylesheet">
 <!--  Custom Scroll bar-->
-<link href="{{asset('public/assets/plugins/mscrollbar/jquery.mCustomScrollbar.css')}}" rel="stylesheet"/>
+<link href="{{asset('assets/plugins/mscrollbar/jquery.mCustomScrollbar.css')}}" rel="stylesheet"/>
 <!--  Sidebar css -->
-{{-- <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous"> --}}
-{{-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.min.js" integrity="sha384-ODmDIVzN+pFdexxHEHFBQH3/9/vQ9uori45z4JjnFsRydbmQbmL5t1tQ0culUzyK" crossorigin="anonymous"></script> --}}
-<link href="{{asset('public/assets/plugins/sidebar/sidebar.css')}}" rel="stylesheet">
+<link href="{{asset('assets/plugins/sidebar/sidebar.css')}}" rel="stylesheet">
 <!-- Sidemenu css -->
-<link rel="stylesheet" href="{{asset('public/assets/css-rtl/sidemenu.css')}}">
+<link rel="stylesheet" href="{{asset('assets/css-rtl/sidemenu.css')}}">
 @yield('css')
 <!--- Style css -->
-<link href="{{asset('public/assets/css-rtl/style.css')}}" rel="stylesheet">
+<link href="{{asset('assets/css-rtl/style.css')}}" rel="stylesheet">
 <!--- Dark-mode css -->
-<link href="{{asset('public/assets/css-rtl/style-dark.css')}}" rel="stylesheet">
+<link href="{{asset('assets/css-rtl/style-dark.css')}}" rel="stylesheet">
 <!---Skinmodes css-->
-<link href="{{asset('public/assets/css-rtl/skin-modes.css')}}" rel="stylesheet">
+<link href="{{asset('assets/css-rtl/skin-modes.css')}}" rel="stylesheet">
+
+{{-- <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous"> --}}
+{{-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.min.js" integrity="sha384-ODmDIVzN+pFdexxHEHFBQH3/9/vQ9uori45z4JjnFsRydbmQbmL5t1tQ0culUzyK" crossorigin="anonymous"></script> --}}
