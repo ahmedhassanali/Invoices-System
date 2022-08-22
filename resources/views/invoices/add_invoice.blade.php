2e73d0e2bdeaea1ed62aa4f34821330229b30155
unknown <ahmeedhassanali@outlook.com>
2022-08-22 20:33:43 +0200

    css

--- resources/views/invoices/add_invoice.blade.php
+++ resources/views/invoices/add_invoice.blade.php
@@ -1,6 +1,24 @@
 @extends('layouts.master')
 
-
+<!-- Title -->
+<title> @yield("title") </title>
+<!-- Favicon -->
+<link rel="icon" href={{asset('assets/img/brand/favicon.png')}} type="image/x-icon"/>
+<!-- Icons css -->
+<link href={{asset('assets/css/icons.css')}} rel="stylesheet">
+<!--  Custom Scroll bar-->
+<link href={{asset('assets/plugins/mscrollbar/jquery.mCustomScrollbar.css')}} rel="stylesheet"/>
+<!--  Sidebar css -->
+<link href={{asset('assets/plugins/sidebar/sidebar.css')}} rel="stylesheet">
+<!-- Sidemenu css -->
+<link rel="stylesheet" href={{asset('assets/css-rtl/sidemenu.css')}}>
+@yield('css')
+<!--- Style css -->
+<link href={{asset('assets/css-rtl/style.css')}} rel="stylesheet">
+<!--- Dark-mode css -->
+<link href={{asset('assets/css-rtl/style-dark.css')}} rel="stylesheet">
+<!---Skinmodes css-->
+<link href={{asset('assets/css-rtl/skin-modes.css')}} rel="stylesheet">
 
 @section('css')
 
@@ -222,6 +240,43 @@
      <!-- Internal form-elements js -->
      <script src="{{ URL::asset('assets/js/form-elements.js') }}"></script>
 
+
+
+
+
+     <!-- Back-to-top -->
+<a href="#top" id="back-to-top"><i class="las la-angle-double-up"></i></a>
+<!-- JQuery min js -->
+<script src="{{asset('assets/plugins/jquery/jquery.min.js')}}"></script>
+<!-- Bootstrap Bundle js -->
+<script src="{{asset('assets/plugins/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+<!-- Ionicons js -->
+<script src="{{asset('assets/plugins/ionicons/ionicons.js')}}"></script>
+<!-- Moment js -->
+<script src="{{asset('assets/plugins/moment/moment.js')}}"></script>
+
+<!-- Rating js-->
+<script src="{{asset('assets/plugins/rating/jquery.rating-stars.js')}}"></script>
+<script src="{{asset('assets/plugins/rating/jquery.barrating.js')}}"></script>
+
+<!--Internal  Perfect-scrollbar js -->
+<script src="{{asset('assets/plugins/perfect-scrollbar/perfect-scrollbar.min.js')}}"></script>
+<script src="{{asset('assets/plugins/perfect-scrollbar/p-scroll.js')}}"></script>
+<!--Internal Sparkline js -->
+<script src="{{asset('assets/plugins/jquery-sparkline/jquery.sparkline.min.js')}}"></script>
+<!-- Custom Scroll bar Js-->
+<script src="{{asset('assets/plugins/mscrollbar/jquery.mCustomScrollbar.concat.min.js')}}"></script>
+<!-- right-sidebar js -->
+<script src="{{asset('assets/plugins/sidebar/sidebar-rtl.js')}}"></script>
+<script src="{{asset('assets/plugins/sidebar/sidebar-custom.js')}}"></script>
+<!-- Eva-icons js -->
+<script src="{{asset('assets/js/eva-icons.min.js')}}"></script>
+@yield('js')
+<!-- Sticky js -->
+<script src="{{asset('assets/js/sticky.js')}}"></script>
+<!-- custom js -->
+<script src="{{asset('assets/js/custom.js')}}"></script><!-- Left-menu js-->
+<script src="{{asset('assets/plugins/side-menu/sidemenu.js')}}"></script>
     <script>
         var date = $('.fc-datepicker').datepicker({
             dateFormat: 'yy-mm-dd'
