69763e1a9c2dc37cf0e30435e31707b7d12b779b
unknown <ahmeedhassanali@outlook.com>
2022-08-22 22:21:22 +0200

    css

--- resources/views/layouts/main-sidebar.blade.php
+++ resources/views/layouts/main-sidebar.blade.php
@@ -34,6 +34,8 @@
 							<span class="side-menu__label">الفواتير</span><i class="angle fe fe-chevron-down"></i></a>
 						<ul class="slide-menu">
 							<li><a class="slide-item" href="{{ url('/' . $page='invoices') }}">قائمة الفواتير</a></li>
+							<li><a class="slide-item" href="{{ url('/' . $page='invoices/create') }}">اضافة فاتورة</a></li>
+
 							<li><a class="slide-item" href="{{ url('/' . $page='invoice_paid') }}">الفواتير المدفوعة</a></li>
 							<li><a class="slide-item" href="{{ url('/' . $page='invoice_unpaid') }}">الفواتير الغير مدفوعة</a></li>
 							<li><a class="slide-item" href="{{ url('/' . $page='invoice_partial') }}">الفواتير المدفوعة جزئيا</a></li>
