2d03862c718375cc74a4c1e7d11812e8b964799c
unknown <ahmeedhassanali@outlook.com>
2022-08-22 22:26:24 +0200

    css

--- resources/views/invoices/add_invoice.blade.php
+++ resources/views/invoices/add_invoice.blade.php
@@ -3,15 +3,15 @@
 @section('css')
 
    <!--- Internal Select2 css-->
-   <link href="{{ URL::asset('assets/plugins/select2/css/select2.min.css') }}" rel="stylesheet">
+   <link href={{ 'assets/plugins/select2/css/select2.min.css' }} rel="stylesheet">
    <!---Internal Fileupload css-->
-   <link href="{{ URL::asset('assets/plugins/fileuploads/css/fileupload.css') }}" rel="stylesheet" type="text/css" />
+   <link href={{'assets/plugins/fileuploads/css/fileupload.css' }} rel="stylesheet" type="text/css" />
    <!---Internal Fancy uploader css-->
-   <link href="{{ URL::asset('assets/plugins/fancyuploder/fancy_fileupload.css') }}" rel="stylesheet" />
+   <link href={{'assets/plugins/fancyuploder/fancy_fileupload.css' }} rel="stylesheet" />
    <!--Internal Sumoselect css-->
-   <link rel="stylesheet" href="{{ URL::asset('assets/plugins/sumoselect/sumoselect-rtl.css') }}">
+   <link rel="stylesheet" href={{'assets/plugins/sumoselect/sumoselect-rtl.css'}}>
    <!--Internal  TelephoneInput css-->
-   <link rel="stylesheet" href="{{ URL::asset('assets/plugins/telephoneinput/telephoneinput-rtl.css') }}">
+   <link rel="stylesheet" href={{'assets/plugins/telephoneinput/telephoneinput-rtl.css'}}>
 
    
 @endsection
