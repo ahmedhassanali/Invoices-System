9dac00d39f2cb12e6d6f1d80c2be9cfcb67c72e0
unknown <ahmeedhassanali@outlook.com>
2022-08-23 17:41:40 +0200

    css

--- resources/views/layouts/head.blade.php
+++ resources/views/layouts/head.blade.php
@@ -1,22 +1,22 @@
 <!-- Title -->
 <title> @yield("title") </title>
 <!-- Favicon -->
-<link rel="icon" href="{{asset('assets/img/brand/favicon.png')}}" type="image/x-icon"/>
+<link rel="icon" href={{asset('assets/img/brand/favicon.png')}} type="image/x-icon"/>
 <!-- Icons css -->
-<link href="{{asset('assets/css/icons.css')}}"  rel="stylesheet">
+<link href={{asset('assets/css/icons.css')}}  rel="stylesheet">
 <!--  Custom Scroll bar-->
-<link href="{{asset('assets/plugins/mscrollbar/jquery.mCustomScrollbar.css')}}" rel="stylesheet"/>
+<link href={{asset('assets/plugins/mscrollbar/jquery.mCustomScrollbar.css')}} rel="stylesheet"/>
 <!--  Sidebar css -->
-<link href="{{asset('assets/plugins/sidebar/sidebar.css')}}" rel="stylesheet">
+<link href={{asset('assets/plugins/sidebar/sidebar.css')}} rel="stylesheet">
 <!-- Sidemenu css -->
-<link href="{{asset('assets/css-rtl/sidemenu.css')}}" rel="stylesheet" >
+<link href={{asset('assets/css-rtl/sidemenu.css')}} rel="stylesheet" >
 @yield('css')
 <!--- Style css -->
-<link href="{{asset('assets/css-rtl/style.css')}}" rel="stylesheet">
+<link href={{asset('assets/css-rtl/style.css')}} rel="stylesheet">
 <!--- Dark-mode css -->
-<link href="{{asset('assets/css-rtl/style-dark.css')}}" rel="stylesheet">
+<link href={{asset('assets/css-rtl/style-dark.css')}} rel="stylesheet">
 <!---Skinmodes css-->
-<link href="{{asset('assets/css-rtl/skin-modes.css')}}" rel="stylesheet">
+<link href={{asset('assets/css-rtl/skin-modes.css')}} rel="stylesheet">
  <!-- Icons css -->
  <!--  Custom Scroll bar-->
  <!--  Sidebar css -->
