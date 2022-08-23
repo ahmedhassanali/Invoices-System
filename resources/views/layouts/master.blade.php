e4c628e114d883b4ba728cdd1dbaab6a439e2c08
unknown <ahmeedhassanali@outlook.com>
2022-08-23 16:46:26 +0200

    css

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -13,8 +13,8 @@
 	<body class="main-body app sidebar-mini">
 		<!-- Loader -->
 		<div id="global-loader">
-			<img src="{{URL::asset('assets/img/loader.svg')}}" class="loader-img" alt="Loader">
-		</div>
+			{{-- <img src="{{URL::asset('assets/img/loader.svg')}}" class="loader-img" alt="Loader">
+		</div> --}}
 		<!-- /Loader -->
 		@include('layouts.main-sidebar')		
 		<!-- main-content -->
