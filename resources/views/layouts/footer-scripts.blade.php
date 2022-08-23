a30405452f3de61748163aa2a4a3fc05c1647ca0
unknown <ahmeedhassanali@outlook.com>
2022-08-23 17:56:21 +0200

    css

--- resources/views/layouts/footer-scripts.blade.php
+++ resources/views/layouts/footer-scripts.blade.php
@@ -10,27 +10,27 @@
 <script src='assets/plugins/moment/moment.js'></script>
 
 <!-- Rating js-->
-<script src='assets/plugins/rating/jquery.rating-stars.js'></script>
-<script src='assets/plugins/rating/jquery.barrating.js'></script>
+<script src='/assets/plugins/rating/jquery.rating-stars.js'></script>
+<script src='/assets/plugins/rating/jquery.barrating.js'></script>
 
 <!--Internal  Perfect-scrollbar js -->
-<script src='assets/plugins/perfect-scrollbar/perfect-scrollbar.min.js'></script>
-<script src='assets/plugins/perfect-scrollbar/p-scroll.js'></script>
+<script src='/assets/plugins/perfect-scrollbar/perfect-scrollbar.min.js'></script>
+<script src='/assets/plugins/perfect-scrollbar/p-scroll.js'></script>
 <!--Internal Sparkline js -->
-<script src='assets/plugins/jquery-sparkline/jquery.sparkline.min.js'></script>
+<script src='/assets/plugins/jquery-sparkline/jquery.sparkline.min.js'></script>
 <!-- Custom Scroll bar Js-->
-<script src='assets/plugins/mscrollbar/jquery.mCustomScrollbar.concat.min.js'></script>
+<script src='/assets/plugins/mscrollbar/jquery.mCustomScrollbar.concat.min.js'></script>
 <!-- right-sidebar js -->
-<script src='assets/plugins/sidebar/sidebar-rtl.js'></script>
-<script src='assets/plugins/sidebar/sidebar-custom.js'></script>
+<script src='/assets/plugins/sidebar/sidebar-rtl.js'></script>
+<script src='/assets/plugins/sidebar/sidebar-custom.js'></script>
 <!-- Eva-icons js -->
-<script src='assets/js/eva-icons.min.js'></script>
+<script src='/assets/js/eva-icons.min.js'></script>
 @yield('js')
 <!-- Sticky js -->
-<script src='assets/js/sticky.js'></script>
+<script src='/assets/js/sticky.js'></script>
 <!-- custom js -->
-<script src='assets/js/custom.js'></script><!-- Left-menu js-->
-<script src='assets/plugins/side-menu/sidemenu.js'></script>
+<script src='/assets/js/custom.js'></script><!-- Left-menu js-->
+<script src='/assets/plugins/side-menu/sidemenu.js'></script>
 
 
 
