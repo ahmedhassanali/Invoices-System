8e2b16e85bdea0e932b344345418a611230c50a4
unknown <ahmeedhassanali@outlook.com>
2022-08-23 17:46:17 +0200

    css

--- resources/views/invoices/add_invoice.blade.php
+++ resources/views/invoices/add_invoice.blade.php
@@ -3,20 +3,7 @@
 
 @section('css')
 
-    <!-- Icons css -->
-    <link href="{{asset('assets/css/icons.css')}}"  rel="stylesheet">
-    <!--  Custom Scroll bar-->
-    <link href="{{asset('assets/plugins/mscrollbar/jquery.mCustomScrollbar.css')}}" rel="stylesheet"/>
-    <!--  Sidebar css -->
-    <link href="{{asset('assets/plugins/sidebar/sidebar.css')}}" rel="stylesheet">
-    <!-- Sidemenu css -->
-    <link href="{{asset('assets/css-rtl/sidemenu.css')}}" rel="stylesheet" >
-    <!--- Style css -->
-    <link href="{{asset('assets/css-rtl/style.css')}}" rel="stylesheet">
-    <!--- Dark-mode css -->
-    <link href="{{asset('assets/css-rtl/style-dark.css')}}" rel="stylesheet">
-    <!---Skinmodes css-->
-    <link href="{{asset('assets/css-rtl/skin-modes.css')}}" rel="stylesheet">
+   
 
    <!--- Internal Select2 css-->
    <link href={{ 'assets/plugins/select2/css/select2.min.css' }} rel="stylesheet">
--- resources/views/layouts/head.blade.php
+++ resources/views/layouts/head.blade.php
@@ -1,20 +1,20 @@
 <!-- Title -->
 <title> @yield("title") </title>
 <!-- Favicon -->
-<link rel="icon" href='assets/img/brand/favicon.png' type="image/x-icon"/>
+<link rel="icon" href='/assets/img/brand/favicon.png' type="image/x-icon"/>
 <!-- Icons css -->
-<link href='assets/css/icons.css'rel="stylesheet">
+<link href='/assets/css/icons.css'rel="stylesheet">
 <!--  Custom Scroll bar-->
-<link href='assets/plugins/mscrollbar/jquery.mCustomScrollbar.css' rel="stylesheet"/>
+<link href='/assets/plugins/mscrollbar/jquery.mCustomScrollbar.css' rel="stylesheet"/>
 <!--  Sidebar css -->
-<link href='assets/plugins/sidebar/sidebar.css' rel="stylesheet">
+<link href='/assets/plugins/sidebar/sidebar.css' rel="stylesheet">
 <!-- Sidemenu css -->
-<link rel="stylesheet" href='assets/css-rtl/sidemenu.css'>
+<link rel="stylesheet" href='/assets/css-rtl/sidemenu.css'>
 @yield('css')
 <!--- Style css -->
-<link href='assets/css-rtl/style.css' rel="stylesheet">
+<link href='/assets/css-rtl/style.css' rel="stylesheet">
 <!--- Dark-mode css -->
-<link href='assets/css-rtl/style-dark.css' rel="stylesheet">
+<link href='/assets/css-rtl/style-dark.css' rel="stylesheet">
 <!---Skinmodes css-->
-<link href='assets/css-rtl/skin-modes.css' rel="stylesheet">
+<link href='/assets/css-rtl/skin-modes.css' rel="stylesheet">
 
