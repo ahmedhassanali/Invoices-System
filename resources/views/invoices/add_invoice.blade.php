b6373e8bf978a47c978736db54f0c9e79876388d
unknown <ahmeedhassanali@outlook.com>
2022-08-23 17:24:13 +0200

    css

--- resources/views/invoices/add_invoice.blade.php
+++ resources/views/invoices/add_invoice.blade.php
@@ -4,19 +4,19 @@
 @section('css')
 
     <!-- Icons css -->
-    <link href=" {{URL::asset('assets/css/icons.css')}}"  rel="stylesheet">
+    <link href='assets/css/icons.css'  rel="stylesheet">
     <!--  Custom Scroll bar-->
-    <link href=" {{URL::asset('assets/plugins/mscrollbar/jquery.mCustomScrollbar.css')}}" rel="stylesheet"/>
+    <link href='/assets/plugins/mscrollbar/jquery.mCustomScrollbar.css' rel="stylesheet"/>
     <!--  Sidebar css -->
-    <link href=" {{URL::asset('assets/plugins/sidebar/sidebar.css')}}" rel="stylesheet">
+    <link href='/assets/plugins/sidebar/sidebar.css' rel="stylesheet">
     <!-- Sidemenu css -->
-    <link href=" {{URL::asset('assets/css-rtl/sidemenu.css')}}" rel="stylesheet" >
+    <link href='/assets/css-rtl/sidemenu.css' rel="stylesheet" >
     <!--- Style css -->
-    <link href=" {{URL::asset('assets/css-rtl/style.css')}}" rel="stylesheet">
+    <link href='/assets/css-rtl/style.css' rel="stylesheet">
     <!--- Dark-mode css -->
-    <link href=" {{URL::asset('assets/css-rtl/style-dark.css')}}" rel="stylesheet">
+    <link href='/assets/css-rtl/style-dark.css' rel="stylesheet">
     <!---Skinmodes css-->
-    <link href=" {{URL::asset('assets/css-rtl/skin-modes.css')}}" rel="stylesheet">
+    <link href='/assets/css-rtl/skin-modes.css' rel="stylesheet">
 
    <!--- Internal Select2 css-->
    <link href={{ 'assets/plugins/select2/css/select2.min.css' }} rel="stylesheet">
