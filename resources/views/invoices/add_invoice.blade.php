1b779fb59759e4d5dbaced3d6b62b27444faaa01
unknown <ahmeedhassanali@outlook.com>
2022-08-28 21:32:55 +0200

    css

--- resources/views/invoices/add_invoice.blade.php
+++ resources/views/invoices/add_invoice.blade.php
@@ -211,7 +211,8 @@
     <script src='/assets/plugins/spectrum-colorpicker/spectrum.js'></script>
     <!-- Internal form-elements js -->
     <script src='/assets/js/form-elements.js'></script>
-
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
     <script>
         var date = $('.fc-datepicker').datepicker({
             dateFormat: 'yy-mm-dd'
