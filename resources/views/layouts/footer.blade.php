6eb328389401608f3b8605a1d98663e2b0386410
unknown <ahmeedhassanali@outlook.com>
2022-08-30 15:29:47 +0200

    css

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,7 +1,7 @@
 <!-- Footer opened -->
 	<div class="main-footer ht-40">
 		<div class="container-fluid pd-t-0-f ht-100p">
-			<span>Copyright © 2022 <a href="www.linkedin.com/in/ahmeedhassanali">Ahmed Hassn Ali</a>All rights reserved.</span>
+			<span>Copyright © 2022 <a href="https://www.linkedin.com/in/ahmeedhassanali">Ahmed Hassn Ali</a> All rights reserved.</span>
 		</div>
 	</div>
 <!-- Footer closed -->
