2b1770ce70d81d7ac84a5c2d5fc2e609a491010b
unknown <ahmeedhassanali@outlook.com>
2022-08-30 16:03:19 +0200

    css

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,37 +1,45 @@
 <!-- Footer opened -->
-<div class="main-footer ht-40">
-    <div class="container-fluid pd-t-0-f ht-100p">
+<div class="main-footer ht-50">
+    <footer class="  text-center my-3">
 
-        
-        <span>
-            <a href="https://www.facebook.com/ahmmedhassanali">
-                <img src="{{ asset('assets/img/icons/f.png') }}" style="width: 20px" class=" mx-2 nav-icon">
-            </a>
-        </span>
 
-        <span>
-            <a href="https://www.instagram.com/ahmed_h.ali/">
-                <img src="{{ asset('assets/img/icons/i.png') }}" style="width: 20px" class=" mx-2 nav-icon">
-            </a>
-        </span>
-        <span>
-            <a href="https://www.behance.net/Ahmed_HAli">
-                <img src="{{ asset('assets/img/icons/b.png') }}" style="width: 20px" class=" mx-2 nav-icon">
-            </a>
-        </span>
-		<span>
-            <a href="https://www.linkedin.com/in/ahmeedhassanali">
-                <img src="{{ asset('assets/img/icons/l.png') }}" style="width: 20px" class=" mx-2 nav-icon">
-            </a>
-        </span>
-        <span>
-            <a href="https://www.github.com/ahmedhassanali">
-                <img src="{{ asset('assets/img/icons/g.png') }}" style="width: 20px" class=" mx-2 nav-icon">
-            </a>
-        </span>
-        <span>Copyright © 2022 <a href="https://www.linkedin.com/in/ahmeedhassanali">Ahmed Hassn Ali</a> All rights
-            reserved.</span>
-    </div>
-</div>
+		{{-- <div class="my-2"> --}}
+
+            <span>
+                <a href="https://www.facebook.com/ahmmedhassanali">
+                    <img src="{{ asset('assets/img/icons/f.png') }}" style="width: 20px" class=" mx-2 nav-icon">
+                </a>
+            </span>
+
+            <span>
+                <a href="https://www.instagram.com/ahmed_h.ali/">
+                    <img src="{{ asset('assets/img/icons/i.png') }}" style="width: 20px" class=" mx-2 nav-icon">
+                </a>
+            </span>
+            <span>
+                <a href="https://www.behance.net/Ahmed_HAli">
+                    <img src="{{ asset('assets/img/icons/b.png') }}" style="width: 20px" class=" mx-2 nav-icon">
+                </a>
+            </span>
+            <span>
+                <a href="https://www.linkedin.com/in/ahmeedhassanali">
+                    <img src="{{ asset('assets/img/icons/l.png') }}" style="width: 20px" class=" mx-2 nav-icon">
+                </a>
+            </span>
+            <span>
+                <a href="https://www.github.com/ahmedhassanali">
+                    <img src="{{ asset('assets/img/icons/g.png') }}" style="width: 20px" class=" mx-2 nav-icon">
+                </a>
+            </span>
+
+        {{-- </div> --}}
+        {{-- <div class="my-2"> --}}
+            <span>Copyright © 2022 <a href="https://www.linkedin.com/in/ahmeedhassanali">Ahmed Hassn Ali</a></span>
+        {{-- </div> --}}
+
+       
+
+
+    </footer>
 </div>
 <!-- Footer closed -->
