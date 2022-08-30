684b1feee934a0397014564dd3c238f3939359b9
unknown <ahmeedhassanali@outlook.com>
2022-08-30 16:13:07 +0200

    css

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,45 +1,40 @@
 <!-- Footer opened -->
 <div class="main-footer ht-50">
-    <footer class="  text-center my-3">
+    <div class="container-fluid pd-t-1-f ht-100p">
 
-
-		{{-- <div class="my-2"> --}}
-
-            <span>
-                <a href="https://www.facebook.com/ahmmedhassanali">
-                    <img src="{{ asset('assets/img/icons/f.png') }}" style="width: 20px" class=" mx-2 nav-icon">
-                </a>
-            </span>
-
-            <span>
-                <a href="https://www.instagram.com/ahmed_h.ali/">
-                    <img src="{{ asset('assets/img/icons/i.png') }}" style="width: 20px" class=" mx-2 nav-icon">
-                </a>
-            </span>
-            <span>
+		
+		
+		<span>
+			<a href="https://www.facebook.com/ahmmedhassanali">
+				<img src="{{ asset('assets/img/icons/f.png') }}" style="width: 15px" class=" mx-2 nav-icon">
+			</a>
+		</span>
+		
+		<span>
+			<a href="https://www.instagram.com/ahmed_h.ali/">
+				<img src="{{ asset('assets/img/icons/i.png') }}" style="width: 15px" class=" mx-2 nav-icon">
+			</a>
+		</span>
+		<span>
                 <a href="https://www.behance.net/Ahmed_HAli">
-                    <img src="{{ asset('assets/img/icons/b.png') }}" style="width: 20px" class=" mx-2 nav-icon">
+                    <img src="{{ asset('assets/img/icons/b.png') }}" style="width: 15px" class=" mx-2 nav-icon">
                 </a>
             </span>
             <span>
-                <a href="https://www.linkedin.com/in/ahmeedhassanali">
-                    <img src="{{ asset('assets/img/icons/l.png') }}" style="width: 20px" class=" mx-2 nav-icon">
+				<a href="https://www.linkedin.com/in/ahmeedhassanali">
+                    <img src="{{ asset('assets/img/icons/l.png') }}" style="width: 15px" class=" mx-2 nav-icon">
                 </a>
             </span>
             <span>
-                <a href="https://www.github.com/ahmedhassanali">
-                    <img src="{{ asset('assets/img/icons/g.png') }}" style="width: 20px" class=" mx-2 nav-icon">
+				<a href="https://www.github.com/ahmedhassanali">
+                    <img src="{{ asset('assets/img/icons/g.png') }}" style="width: 15px" class=" mx-2 nav-icon">
                 </a>
             </span>
-
-        {{-- </div> --}}
-        {{-- <div class="my-2"> --}}
-            <span>Copyright © 2022 <a href="https://www.linkedin.com/in/ahmeedhassanali">Ahmed Hassn Ali</a></span>
-        {{-- </div> --}}
-
-       
-
-
-    </footer>
+			
+			
+			
+			<span>Copyright © 2022 <a href="https://www.linkedin.com/in/ahmeedhassanali">Ahmed Hassan Ali.</a></span>
+    </div>
+</div>
 </div>
 <!-- Footer closed -->
