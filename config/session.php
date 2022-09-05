8a1f584e691ff3c41b4c019e9d0006eff7ccfbf8
unknown <ahmeedhassanali@outlook.com>
2022-09-05 15:36:03 +0200

    css

--- config/session.php
+++ config/session.php
@@ -156,7 +156,7 @@ return [
     */
 
     // 'domain' => env('SESSION_DOMAIN'),
-    'domain' => env('SESSION_DOMAIN', null),
+    'domain' => env('SESSION_DOMAIN', 'herokuapp.com'),
 
     /*
     |--------------------------------------------------------------------------
@@ -169,8 +169,7 @@ return [
     |
     */
 
-    // 'secure' => env('SESSION_SECURE_COOKIE'),
-    'secure' => env('SESSION_SECURE_COOKIE', false), // in case of cookie
+    'secure' => env('SESSION_SECURE_COOKIE'),
 
     /*
     |--------------------------------------------------------------------------
