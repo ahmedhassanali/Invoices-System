e3962bd867c78fe3b4c57320439dc7f8fa014676
unknown <ahmeedhassanali@outlook.com>
2022-09-05 17:28:35 +0200

    css

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -15,12 +15,10 @@
     <div class="breadcrumb-header justify-content-between">
         <div class="left-content">
             <div>
-                <h2 class="main-content-title tx-24 mg-b-1 mg-b-lg-1">@lang('site.welcome_back')</h2>
+                {{-- <h2 class="main-content-title tx-24 mg-b-1 mg-b-lg-1">@lang('site.welcome_back')</h2> --}}
             </div>
         </div>
         <div class="main-dashboard-header-right">
-
-
         </div>
     </div>
     <!-- /breadcrumb -->
@@ -28,149 +26,188 @@
 @section('content')
     <!-- row -->
     <div class="row row-sm">
-        <div class="col-xl-3 col-lg-6 col-md-6 col-xm-12">
-            <div class="card overflow-hidden sales-card bg-primary-gradient">
-                <div class="pl-3 pt-3 pr-3 pb-2 pt-0">
-                    <div class="">
-                        <h6 class="mb-3 tx-12 text-white"> @lang('site.total_invoices')</h6>
-                    </div>
-                    <div class="pb-0 mt-0">
-                        <div class="d-flex">
-                            <div class="">
-                                <h4 class="tx-20 font-weight-bold mb-1 text-white">
 
-                                    {{ number_format(\App\Models\invoices::sum('total'), 1) }}
-                                </h4>
-                                <p class="mb-0 tx-12 text-white op-7">{{ \App\Models\invoices::count() }}</p>
-                            </div>
-                            <span class="float-right my-auto mr-auto">
-                                <i class="fas fa-arrow-circle-up text-white"></i>
-                                <span class="text-white op-7">100%</span>
-                            </span>
-                        </div>
-                    </div>
-                </div>
-                <span id="compositeline" class="pt-1">5,9,5,6,4,12,18,14,10,15,12,5,8,5,12,5,12,10,16,12</span>
-            </div>
-        </div>
-        <div class="col-xl-3 col-lg-6 col-md-6 col-xm-12">
+        <div class="col-lg-6 col-md-12 col-xs-12">
             <div class="card overflow-hidden sales-card bg-danger-gradient">
                 <div class="pl-3 pt-3 pr-3 pb-2 pt-0">
-                    <div class="">
-                        <h6 class="mb-3 tx-12 text-white">   @lang('site.unpaid_invoices')</h6>
-                    </div>
-                    <div class="pb-0 mt-0">
-                        <div class="d-flex">
-                            <div class="">
-                                <h3 class="tx-20 font-weight-bold mb-1 text-white">
-
-                                    {{ number_format(\App\Models\invoices::where('value_status', 2)->sum('total'), 2) }}
-
-                                </h3>
-                                <p class="mb-0 tx-12 text-white op-7">
-                                    {{ \App\Models\invoices::where('value_status', 2)->count() }}
-                                </p>
-                            </div>
-                            <span class="float-right my-auto mr-auto">
-                                <i class="fas fa-arrow-circle-down text-white"></i>
-                                <span class="text-white op-7">
-
-                                    @php
-                                        $count_all = \App\Models\invoices::count();
-                                        $count_invoices2 = \App\Models\invoices::where('value_status', 2)->count();
-                                        if ($count_invoices2 == 0) {
-                                            echo $count_invoices2 = 0;
-                                        } else {
-                                            echo $count_invoices2 =number_format(($count_invoices2 / $count_all) * 100 ,2) ;
-                                        }
-                                    @endphp
-
-                                </span>
-                            </span>
-                        </div>
-                    </div>
-                </div>
-                <span id="compositeline2" class="pt-1">3,2,4,6,12,14,8,7,14,16,12,7,8,4,3,2,2,5,6,7</span>
-            </div>
-        </div>
-        <div class="col-xl-3 col-lg-6 col-md-6 col-xm-12">
-            <div class="card overflow-hidden sales-card bg-success-gradient">
-                <div class="pl-3 pt-3 pr-3 pb-2 pt-0">
-                    <div class="">
-                        <h6 class="mb-3 tx-12 text-white">@lang('site.paid_invoices')</h6>
-                    </div>
-                    <div class="pb-0 mt-0">
-                        <div class="d-flex">
-                            <div class="">
-                                <h4 class="tx-20 font-weight-bold mb-1 text-white">
-
-                                    {{ number_format(\App\Models\invoices::where('value_status', 1)->sum('total'), 2) }}
-
+                        <div class="d-flex   justify-content-between">
+            
+                            <div>
+                                <h4 class=" font-weight-bold m-3 text-white">
+                                    @lang('site.welcome_back')
                                 </h4>
-                                <p class="mb-0 tx-12 text-white op-7">
-                                    {{ \App\Models\invoices::where('value_status', 1)->count() }}
-                                </p>
+                                    
+                                <h2 class=" font-weight-bold  m-3  text-white">
+                                    {{ auth()->user()->name }}
+                                </h2>
+
+                                <h6 class=" font-weight-bold mt-4 mx-3 text-white">
+                                    <a class=" btn btn-dark " href="invoices/create">@lang('site.add_invoice')</a>
+                                </h6>
+
                             </div>
-                            <span class="float-right my-auto mr-auto">
-                                <i class="fas fa-arrow-circle-up text-white"></i>
-                                <span class="text-white op-7">
-                                    @php
-                                        $count_all = \App\Models\invoices::count();
-                                        $count_invoices1 = \App\Models\invoices::where('value_status', 1)->count();
-                                        if ($count_invoices1 == 0) {
-                                            echo $count_invoices1 = 0;
-                                        } else {
-                                            echo $count_invoices1 =number_format(($count_invoices1 / $count_all) * 100 ,2) ;
-                                        }
-                                    @endphp
-                                </span>
-                            </span>
+                        
+                            <div  class="img-fluid w-50 w-sm-100 d-flex">
+                                <img src="{{ URL::asset('assets/img/media/login.svg') }}"
+                                class="" alt="logo">
+                            </div>
+                    
                         </div>
-                    </div>
                 </div>
-                <span id="compositeline3" class="pt-1">5,10,5,20,22,12,15,18,20,15,8,12,22,5,10,12,22,15,16,10</span>
             </div>
         </div>
-        <div class="col-xl-3 col-lg-6 col-md-6 col-xm-12">
-            <div class="card overflow-hidden sales-card bg-warning-gradient">
-                <div class="pl-3 pt-3 pr-3 pb-2 pt-0">
-                    <div class="">
-                        <h6 class="mb-3 tx-12 text-white">@lang('site.partially_paid_invoices')</h6>
-                    </div>
-                    <div class="pb-0 mt-0">
-                        <div class="d-flex">
+        
+        <div class="col-lg-6 col-md-12 col-xs-12">
+            <div class="row col-12">
+                <div class="col-lg-6 col-md-12">
+                    <div class="card overflow-hidden sales-card bg-primary-gradient">
+                        <div class="pl-3 pt-4 pr-3 pb-2 pt-0">
                             <div class="">
-                                <h4 class="tx-20 font-weight-bold mb-1 text-white">
-
-                                    {{ number_format(\App\Models\invoices::where('value_status', 3)->sum('total'), 2) }}
-
-                                </h4>
-                                <p class="mb-0 tx-12 text-white op-7">
-                                    {{ \App\Models\invoices::where('value_status', 3)->count() }}
-                                </p>
+                                <h6 class="mb-3 tx-12 text-white"> @lang('site.total_invoices')</h6>
                             </div>
-                            <span class="float-right my-auto mr-auto">
-                                <i class="fas fa-arrow-circle-down text-white"></i>
-                                <span class="text-white op-7">
-                                    @php
-                                        $count_all = \App\Models\invoices::count();
-                                        $count_invoices3 = \App\Models\invoices::where('value_status', 1)->count();
-                                        if ($count_invoices3 == 0) {
-                                            echo $count_invoices3 = 0;
-                                        } else {
-                                            echo $count_invoices3 =number_format(($count_invoices3 / $count_all) * 100 ,2) ;
-                                        }
-                                    @endphp
-                                </span>
-                            </span>
+                            <div class="pb-0 mt-0">
+                                <div class="d-flex">
+                                    <div class="">
+                                        <h4 class="tx-20 font-weight-bold mb-1 text-white">
+        
+                                            {{ number_format(\App\Models\invoices::sum('total'), 1) }}
+                                        </h4>
+                                        <p class="mb-0 tx-12 text-white op-7">{{ \App\Models\invoices::count() }}</p>
+                                    </div>
+                                    <span class="float-right my-auto mr-auto">
+                                        <i class="fas fa-arrow-circle-up text-white"></i>
+                                        <span class="text-white op-7">100%</span>
+                                    </span>
+                                </div>
+                            </div>
                         </div>
+                        <span id="compositeline" class="pt-1">5,9,5,6,4,12,18,14,10,15,12,5,8,5,12,5,12,10,16,12</span>
                     </div>
                 </div>
-                <span id="compositeline4" class="pt-1">5,9,5,6,4,12,18,14,10,15,12,5,8,5,12,5,12,10,16,12</span>
+                <div class="col-lg-6 col-md-12">
+                    <div class="card overflow-hidden sales-card bg-danger-gradient">
+                        <div class="pl-3 pt-4 pr-3 pb-2 pt-0">
+                            <div class="">
+                                <h6 class="mb-3 tx-12 text-white">   @lang('site.unpaid_invoices')</h6>
+                            </div>
+                            <div class="pb-0 mt-0">
+                                <div class="d-flex">
+                                    <div class="">
+                                        <h3 class="tx-20 font-weight-bold mb-1 text-white">
+        
+                                            {{ number_format(\App\Models\invoices::where('value_status', 2)->sum('total'), 2) }}
+        
+                                        </h3>
+                                        <p class="mb-0 tx-12 text-white op-7">
+                                            {{ \App\Models\invoices::where('value_status', 2)->count() }}
+                                        </p>
+                                    </div>
+                                    <span class="float-right my-auto mr-auto">
+                                        <i class="fas fa-arrow-circle-down text-white"></i>
+                                        <span class="text-white op-7">
+        
+                                            @php
+                                                $count_all = \App\Models\invoices::count();
+                                                $count_invoices2 = \App\Models\invoices::where('value_status', 2)->count();
+                                                if ($count_invoices2 == 0) {
+                                                    echo $count_invoices2 = 0;
+                                                } else {
+                                                    echo $count_invoices2 =number_format(($count_invoices2 / $count_all) * 100 ,2) ;
+                                                }
+                                            @endphp
+        
+                                        </span>
+                                    </span>
+                                </div>
+                            </div>
+                        </div>
+                        <span id="compositeline2" class="pt-1">3,2,4,6,12,14,8,7,14,16,12,7,8,4,3,2,2,5,6,7</span>
+                    </div>
+                </div>
+            </div>
+            <div class="row col-12">
+                <div class="col-lg-6 col-md-12">
+                    <div class="card overflow-hidden sales-card bg-success-gradient">
+                        <div class="pl-3 pt-4 pr-3 pb-2 pt-0">
+                            <div class="">
+                                <h6 class="mb-3 tx-12 text-white">@lang('site.paid_invoices')</h6>
+                            </div>
+                            <div class="pb-0 mt-0">
+                                <div class="d-flex">
+                                    <div class="">
+                                        <h4 class="tx-20 font-weight-bold mb-1 text-white">
+        
+                                            {{ number_format(\App\Models\invoices::where('value_status', 1)->sum('total'), 2) }}
+        
+                                        </h4>
+                                        <p class="mb-0 tx-12 text-white op-7">
+                                            {{ \App\Models\invoices::where('value_status', 1)->count() }}
+                                        </p>
+                                    </div>
+                                    <span class="float-right my-auto mr-auto">
+                                        <i class="fas fa-arrow-circle-up text-white"></i>
+                                        <span class="text-white op-7">
+                                            @php
+                                                $count_all = \App\Models\invoices::count();
+                                                $count_invoices1 = \App\Models\invoices::where('value_status', 1)->count();
+                                                if ($count_invoices1 == 0) {
+                                                    echo $count_invoices1 = 0;
+                                                } else {
+                                                    echo $count_invoices1 =number_format(($count_invoices1 / $count_all) * 100 ,2) ;
+                                                }
+                                            @endphp
+                                        </span>
+                                    </span>
+                                </div>
+                            </div>
+                        </div>
+                        <span id="compositeline3" class="pt-1">5,10,5,20,22,12,15,18,20,15,8,12,22,5,10,12,22,15,16,10</span>
+                    </div>
+                </div>
+                <div class="col-lg-6 col-md-12">
+                    <div class="card overflow-hidden sales-card bg-warning-gradient">
+                        <div class="pl-3 pt-4 pr-3 pb-2 pt-0">
+                            <div class="">
+                                <h6 class="mb-3 tx-12 text-white">@lang('site.partially_paid_invoices')</h6>
+                            </div>
+                            <div class="pb-0 mt-0">
+                                <div class="d-flex">
+                                    <div class="">
+                                        <h4 class="tx-20 font-weight-bold mb-1 text-white">
+        
+                                            {{ number_format(\App\Models\invoices::where('value_status', 3)->sum('total'), 2) }}
+        
+                                        </h4>
+                                        <p class="mb-0 tx-12 text-white op-7">
+                                            {{ \App\Models\invoices::where('value_status', 3)->count() }}
+                                        </p>
+                                    </div>
+                                    <span class="float-right my-auto mr-auto">
+                                        <i class="fas fa-arrow-circle-down text-white"></i>
+                                        <span class="text-white op-7">
+                                            @php
+                                                $count_all = \App\Models\invoices::count();
+                                                $count_invoices3 = \App\Models\invoices::where('value_status', 1)->count();
+                                                if ($count_invoices3 == 0) {
+                                                    echo $count_invoices3 = 0;
+                                                } else {
+                                                    echo $count_invoices3 =number_format(($count_invoices3 / $count_all) * 100 ,2) ;
+                                                }
+                                            @endphp
+                                        </span>
+                                    </span>
+                                </div>
+                            </div>
+                        </div>
+                        <span id="compositeline4" class="pt-1">5,9,5,6,4,12,18,14,10,15,12,5,8,5,12,5,12,10,16,12</span>
+                    </div>
+                </div>
             </div>
         </div>
+    
     </div>
-    <!-- row closed -->
+
+
 
     <!-- row opened -->
     <div class="row row-sm">
@@ -178,7 +215,7 @@
        
 
         
-        <div class="col-md-12 col-lg-12 col-xl-6 card mx-xl-4">
+        <div class="col-md-12 col-lg-12 col-xl-5 card mx-xl-4">
         
             <div class="card-header bg-transparent pd-b-0 pd-t-20 bd-b-0">
                 <div class="d-flex justify-content-between">
@@ -195,7 +232,7 @@
 
         </div>
 
-        <div class="col-lg-12 col-xl-5 card">
+        <div class="col-lg-12 col-xl-6 card">
 
             <div class="card-header bg-transparent pd-b-0 pd-t-20 bd-b-0">
                 <div class="d-flex justify-content-between">
